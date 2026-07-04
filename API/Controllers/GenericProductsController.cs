bf049d5b3774322401bc6ce5ad430f7020be5661
Winged1One <vinithavv888@gmail.com>
2026-07-04 11:14:20 +0530

    Implemented Specification Pattern

---
+++ API/Controllers/GenericProductsController.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using Core.Interfaces;
+using Core.Entities;
+using Core.Specifications;
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class GenericProductsController(IGenericRepository<Product> repo) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
+    {
+        var spec = new ProductSpecification(brand, type, sort);
+        var products = await repo.ListAsync(spec);
+        return Ok(products);
+    }
+    [HttpGet("{id:int}")]  //api/Products/2
+    public async Task<ActionResult<Product>> GetProductById(int id)
+    {
+        if(id <= 0) return BadRequest();
+        var product = await repo.GetByIdAsync(id);
+
+        if(product == null) return NotFound();
+        return product;
+    }
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        repo.Add(product);
+        if (await repo.SaveAllAsync())
+        {
+            return CreatedAtAction("GetProductById", new {id = product.Id}, product);
+        }
+        return BadRequest("Problem creating product");
+    }
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if(product.Id !=id || !ProductExist(id))
+            return BadRequest("Cannot update the product");
+        
+        repo.Update(product);
+        if(await repo.SaveAllAsync())
+        {
+            return NoContent();
+        }
+        return BadRequest("Problem in updating the product");
+    }
+    private bool ProductExist(int id)
+    {
+        return repo.Exist(id);
+    }
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        if(!ProductExist(id))
+            return BadRequest();
+
+        var product = await repo.GetByIdAsync(id);
+        if(product == null ) return NotFound();
+        
+        repo.Remove(product);
+        if(await repo.SaveAllAsync())
+        {
+            return NoContent();
+        }
+        return BadRequest("Problem in deleting the product");
+    }
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        var spec = new BrandListSpecification();
+        return Ok(await repo.ListAsync(spec));
+    }
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        var spec = new TypeListSpecification();
+        return Ok(await repo.ListAsync(spec));
+    }
+}
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+//This means it only accept classes that inherits from BaseEntity
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    Task<TResult?> GetEntityWithSpec<TResult>(ISpecification<T, TResult> spec);
+    //TResult is not declared by the class, so the method must declare it using <TResult>.
+    //The <TResult> after ListAsync declares TResult, and then the return type can use it.
+    Task<IReadOnlyList<TResult>> ListAsync<TResult>(ISpecification<T, TResult> spec);
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<bool> SaveAllAsync();
+    bool Exist(int id);
+}
